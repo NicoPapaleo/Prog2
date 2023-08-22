4176e644f5eba3e851124ae4ba258a983bc93334
Nico Papaleo <nikoopapaleo@gmail.com>
2023-08-22 14:08:26 -0300

    primer commit prog2

---
+++ Prog 2/srcComision 1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Avion air314 = new Avion();
+        System.out.println(air314.getColor());
+        air314.setColor("rojo");
+        System.out.println(air314.getColor());
+    }
+
+}
